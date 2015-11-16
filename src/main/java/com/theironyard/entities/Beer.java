ae1ec36d02e4d8fec4eab5d05a8b6edbc9d5af76
alhanger <hanger129@gmail.com>
2015-11-16 10:45:24 -0500

    Set columns to be non-nullable

--- src/main/java/com/theironyard/entities/Beer.java
+++ src/main/java/com/theironyard/entities/Beer.java
@@ -1,10 +1,7 @@
 package com.theironyard.entities;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 /**
  * Created by alhanger on 11/10/15.
@@ -14,10 +11,16 @@ public class Beer {
 
     @Id
     @GeneratedValue
-    Integer id;
+    @Column(nullable = false)
+    int id;
 
+    @Column(nullable = false)
     public String name;
+
+    @Column(nullable = false)
     public String type;
+
+    @Column(nullable = false)
     public Integer calories;
 
     @ManyToOne
--- src/main/java/com/theironyard/entities/User.java
+++ src/main/java/com/theironyard/entities/User.java
@@ -13,9 +13,13 @@ import java.util.List;
 public class User {
     @Id
     @GeneratedValue
-    Integer id;
+    @Column(nullable = false)
+    int id;
 
+    @Column(nullable = false)
     public String name;
+
+    @Column(nullable = false)
     public String password;
 
     @OneToMany(mappedBy = "user") // this is the name of the field in the beer class
